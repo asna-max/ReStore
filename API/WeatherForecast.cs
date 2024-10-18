01d01f64c2eebb96a013da9e2fe461436512bdc3
asna-max <ashok-lk@hotmail.com>
2024-10-18 11:21:42 +0200

    End of section 2

---
+++ API/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+        public Double TemperatureC { get; set; }
+        public string Summary { get; set; }
+        
+    }
+}
