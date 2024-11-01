0f3b04a8fb906e4efd863c0089dcf8c3dc4eee75
asna-max <81409289+asna-max@users.noreply.github.com>
2024-11-01 14:49:32 +0100

    End od section 3

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,39 @@
+import Catalog from "../../features/catalog/Catalog";
+import { Container, createTheme, ThemeProvider } from "@mui/material";
+import Header from "./Header";
+import { useState } from "react";
+
+
+function App() {
+  const [darkmode, setDarkMode] = useState(false);
+  const paletteType = darkmode ? 'dark' : 'light';
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+      background:{
+        default: paletteType === 'light' ? '#eaeaea' : '#121212'
+      }
+    }
+  });
+
+  function handleThemeChange() {
+    setDarkMode(!darkmode)
+  }
+
+
+  return (
+    <ThemeProvider theme={theme} >
+      <Header darkMode={darkmode} handleThemeChange={handleThemeChange}></Header>
+      <Container>
+        <Catalog />
+      </Container>
+
+    </ThemeProvider>
+
+  );
+}
+
+export default App;
+
+
+
